e7be5525d447e2d08236f982fda8e4b502672c86
Steven Casagrande <scasagrande@galvant.ca>
2016-04-10 20:20:13 -0400

    Initial commit

---
+++ pelicanconf.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Steven Casagrande'
+SITENAME = 'Steven Casagrande'
+SITEURL = ''
+
+PATH = 'content'
+
+THEME = 'theme'
+
+TIMEZONE = 'EST'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('InstrumentKit', 'https://www.github.com/Galvant/InstrumentKit'),
+         ('Galvant Industries', 'http://galvant.ca/'),)
+
+# Social widget
+SOCIAL = (('github', 'http://github.com/scasagrande'),
+          ('twitter', 'https://twitter.com/stevecasagrande'),
+          ('linkedin', 'https://www.linkedin.com/in/steven-casagrande-633ba533'),
+          )
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
